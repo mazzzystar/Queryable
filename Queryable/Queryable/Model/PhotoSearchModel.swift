40d721c2e2fb6c1c16cb656041d472c0f9e64fc7
Ke Fang <myfancoo@qq.com>
2023-07-11 09:32:49 +0800

    remove unused comment.

--- Queryable/Queryable/Model/PhotoSearchModel.swift
+++ Queryable/Queryable/Model/PhotoSearchModel.swift
@@ -10,8 +10,6 @@ import Foundation
 import Accelerate
 
 struct PhotoSearcherModel {
-    // TODO: Seperate TextEncode & ImgEncoder. Because only text encoder is needed
-    // TODO: when searching.
     private var texEncoder: TextEncoder?
     
     mutating func load_text_encoder() {
@@ -30,24 +28,11 @@ struct PhotoSearcherModel {
         return emb!
     }
     
-//    func img_embedding(image: UIImage) -> MLShapedArray<Float32> {
-//        let emb = try! clip.computeImgEmbedding(img: image)
-//        return emb
-//    }
-    
     func cosine_similarity(A: MLShapedArray<Float32>, B: MLShapedArray<Float32>) -> Float {
         let magnitude = vDSP.rootMeanSquare(A.scalars) * vDSP.rootMeanSquare(B.scalars)
         let dotarray = vDSP.dot(A.scalars, B.scalars)
         return  dotarray / magnitude
     }
-    
-    /**
-    # Python version of spherical_dist_loss
-    def spherical_dist_loss(x, y):
-        x = F.normalize(x, dim=-1)
-        y = F.normalize(y, dim=-1)
-        return (x - y).norm(dim=-1).div(2).arcsin().pow(2).mul(2)
-     */
     
     func spherical_dist_loss(A: MLShapedArray<Float32>, B: MLShapedArray<Float32>) async -> Float {
         let a = vDSP.divide(A.scalars, sqrt(vDSP.sumOfSquares(A.scalars)))
@@ -56,13 +41,5 @@ struct PhotoSearcherModel {
         let magnitude = sqrt(vDSP.sumOfSquares(vDSP.subtract(a, b)))
         return pow(asin(magnitude / 2.0), 2) * 2.0
     }
-    
-//    func calculate_emb_for_all_photos(images: [Photo]) -> MLShapedArray<Float32> {
-//        for image in images {
-//            let emb = try! clip.computeImgEmbedding(img: image)
-//        }
-//
-//        return emb
-//    }
 
 }
